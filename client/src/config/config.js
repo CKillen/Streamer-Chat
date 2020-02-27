8e1da82bddf5f2278eec316d3c0b8e729c7550c2
CKillen <cl_killen@hotmail.com>
2020-02-27 04:55:25 -0500

    .gitignore was set up wrong...

--- client/src/config/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    backendURL: 'http://localhost:4050'
-}
-
-export default config;
--- server/config/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    oauth: "oauth:miuvyfz3bolyddzwdmxywjmqsw2na1",
-    nick: "hicures"
-}
-module.exports = config;
